49270b1beb927f42364e7f97afa33ce06b695125
Erik Öberg <erik.oberg@miun.se>
2022-11-02 14:34:03 +0100

    Lagt till kod för föreläsning 3

--- FL3/MainWindow.xaml.cs
+++ FL3/MainWindow.xaml.cs
@@ -25,5 +25,85 @@ namespace FL3
             InitializeComponent();
             // ny kommentar
         }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Styrande satser
+
+            // Måste alltid vara på formen ja eller nej
+
+            // villkor
+
+            //if(villkor) // mitt villkor måste kunna valideras som sant eller falskt
+
+            //string personOne = "Erik Öberg", personTwo="Sara";
+            //if (personOne.Length < 8)
+            //{
+            //    if (personOne.Length > personTwo.Length)
+            //    {
+            //        // Den här koden körs om och endast om
+            //        // villkoret är sant
+            //        MessageBox.Show("Ditt namn är längst");
+            //    }
+            //    else if (personOne.Length == personTwo.Length)
+            //    {
+            //        MessageBox.Show("Era namn är lika långa");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Ditt namn är kortast");
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Ditt namn är för långt");
+            //}
+
+            /*    Har föraren hållit rätt hastighet?
+             *    Är föraren nykter?
+             *    --> om promillehalten < x
+             *      -- rattonykter
+             *      annars rattfull
+             * 
+             */
+            int currentSpeed = 95, speedLimit=90;
+            double toxicLevel = 0.5;
+
+            bool hasCorrectSpeed = currentSpeed <= speedLimit;
+            bool isSober = toxicLevel<0.1;
+
+            if (!hasCorrectSpeed)
+            {
+                // Du har kört olagligt
+            }
+
+            if (toxicLevel >= 0.1)
+            {
+                // rattfylleri
+                if (toxicLevel >= 0.5)
+                {
+                    // grovt rattfull
+                }
+            }
+            
+            if(hasCorrectSpeed && isSober) // OCH
+            {
+                // Bra precis som det ska vara
+            }
+            else
+            {
+                // ajajaj
+            }
+
+
+            if (!hasCorrectSpeed || !isSober) // ELLEr
+            {
+                // Ajajajaj
+            }
+            else
+            {
+                // Bra
+            }
+        }
     }
 }
