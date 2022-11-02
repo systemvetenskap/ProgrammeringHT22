62999b0e075ef344158135a6fc31b64ee0a4b6bb
Erik Öberg <erik.oberg@miun.se>
2022-11-02 13:08:27 +0100

    Add project files.

---
+++ FL1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnMessage_Click(object sender, RoutedEventArgs e)
+        {
+            // ANVÄND DEBUGGER!!!!
+            // Gör att vi kan stega oss igenom koden
+            // 1. Sätt en breakpoint
+
+            // Vi vill hämta text från vår textruta
+           // txtName.Text = "Erik Öberg";
+
+            // Vi vill ofta spara värden till längre fram i koden
+            // gör en variabel
+            // Alla variabler i C# MÅSTE ha en datatyp
+
+            //datatyp + variabelnamn. Namnet ska ALLTID börja med liten bokstav (camelCase)
+            // vi deklarerar variabler
+            string firstName, lastName;
+            // Tilldela värden till våra variabler
+            firstName = txtName.Text;
+            lastName = txtLastname.Text;
+            // Man kan slå ihop strängar i C#
+            // konkatenera
+            // string interpolation
+            MessageBox.Show("Hej" + " " + txtName.Text+ " " + txtLastname.Text);
+            //MessageBox.Show($"Hej {firstName} {lastName}");
+        }
+
+
+    }
+}
---
+++ FL2/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int myLuckyNumber = 4;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // deklarerar variabeln lastname
+            string lastName;
+            // deklarerar och tilldelar
+            string firstName = "Nils";
+
+            lastName=txtName.Text;
+
+            // tilldelar värde <---- görs från höger till vänster
+            firstName += " Erik"; // utöka redan befintliga data i variabeln
+
+           lblResult.Content =  $"{firstName} {lastName}";
+            firstName = "Johanna";
+           txtName.Text =  $"{firstName} {lastName}";
+
+            // Scope
+            // räckvidd
+            // variablers räckvidd
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            
+            // Variabler som kan innehålla siffror
+            // heltal
+            // flyttal, decimaltal
+
+            // datatyp   variabelnamn   och eventuellt startvärde
+            string firstName = "Pelle";
+            // heltal         // sträng omvandlat till ett heltal
+            myLuckyNumber = int.Parse(txtName.Text);
+
+            myLuckyNumber++;
+
+
+            lblResult.Content = myLuckyNumber;
+            // Konvertera heltalet till en sträng
+            // Metoder har ALLTID en parentes!
+            txtName.Text = myLuckyNumber.ToString();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            // hämta ålder och lägg i en variabel
+            int ageInYears, daysPerYear=365, totalDaysLived;
+            double totalWeeksLived, daysInWeek;
+
+
+            // tilldela värde
+            //ageInYears = int.Parse(txtName.Text);
+            //totalDaysLived = ageInYears * daysPerYear;
+
+            daysInWeek = double.Parse(txtName.Text);
+
+            // hur många veckor har jag levt?
+            // Utför en heltalsdivision
+            totalWeeksLived = daysPerYear / 7;
+
+            // vill utföra en flyttalsdivision
+            // cast
+            //totalWeeksLived = daysPerYear / (double)ageInYears;
+
+           
+
+            //totalWeeksLived = 52.2;
+            // Clean Code
+            // refaktorering
+
+            //lblResult.Content=$"Du har totalt levt i {totalDaysLived} antal dagar";
+            lblResult.Content=$"Antal veckor: {daysInWeek}";
+
+
+            
+        }
+    }
+}
